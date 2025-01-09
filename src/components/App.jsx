b51ce11f84c548a0e5dbf47f08655e95006299c1
Oleksandr Prokopchuk <sasha195@i.ua>
2025-01-09 10:29:53 +0200

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,17 +19,17 @@ export default function App() {
         <p>Text between opening and closing tag</p>
       </Card>
 
-      {/* <h1>Books of the week</h1>
-      <BookList books={favouriteBooks} /> */}
+      <h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
 
-      {/* <Mailbox username="Ivan" />
+      <Mailbox username="Ivan" />
       <h1>Best selling</h1>
       <Product name="Tacos With Lime" price={10.99} />
       <Product
         name="Fries and Burger"
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
-      /> */}
+      />
     </div>
   );
 }
